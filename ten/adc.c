ffed3f16c7565fd182e1ae1a213216edfe95787f
imperishability <mail.oleksiy@gmail.com>
2021-04-04 20:13:59 +0300

    Changed config registers to enable internal Vref
    Changed Vref and polynomial linearization

--- ten/adc.c
+++ ten/adc.c
@@ -9,19 +9,19 @@
 #include "adc.h"
 
 float GetVoltage(){
-	ADCSRA|=(1 << ADSC);
+	ADCSRA |= (1 << ADSC);
 	while(ADCSRA & (1 << ADSC));
-	unsigned int ADC_Code = (ADCL|(ADCH << 8));
-	float Voltage = (5 * (float)ADC_Code) / 1024;
+	int ADC_Code = (ADCL|(ADCH << 8));
+	float Voltage = (Vref * (float)ADC_Code) / 1024;
 	return Voltage; 
 }
 
 float GetTemperature(){
 	float voltage = GetVoltage();
-	return (-0.2843 * (voltage * voltage) + 35.085 * voltage - 2.0869);
+	return -0.4749 * (voltage * voltage) + 45.197 * voltage - 10.441;
 }
 
 void ADC_Config(void){
-	ADMUX |= (1 << MUX0)|(1 << MUX1)|(1 << MUX2)|(1 << REFS0);	//adc7
+	ADMUX |= (1 << MUX0)|(1 << MUX1)|(1 << MUX2)|(1 << REFS0)|(1 << REFS1);	//adc7
 	ADCSRA |= (1 << ADPS0)|(1 << ADPS1)|(ADPS2)|(1 << ADEN);
 }
